949146e4c7cab0b4eede9fa48e76f7e4d265cba5
ulysses <ulysses.alessandro@gmail.com>
2016-03-27 16:39:36 -0300

    loading routes list in watch

---
+++ TransitApp WatchKit Extension/RouteRow.swift
@@ -0,0 +1,13 @@
+//
+//  RouteRow.swift
+//  TransitApp
+//
+//  Created by Ulysses on 3/27/16.
+//  Copyright © 2016 Ulysses Rocha. All rights reserved.
+//
+
+import WatchKit
+
+class RouteRow: NSObject {
+
+}
